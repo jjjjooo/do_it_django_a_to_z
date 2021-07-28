1850b5ae2b1373c78ae15ce7077c5a02145cde32
fullb <fullbster@gmail.com>
2021-07-28 22:46:49 +0900

    포스트 수정 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -33,17 +33,61 @@ class TestView(TestCase):
             title='두번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
             category=self.category_music,
-            author=self.user_obama,
+            author=self.user_trump,
         )
 
         self.post_003 = Post.objects.create(
             title='Post Form 만들기',
             content='카테고리가 없다',
-            author=self.user_trump,
+            author=self.user_obama,
         )
         self.post_003.tags.add(self.tag_python_kor)
         self.post_003.tags.add(self.tag_python)
 
+    def test_update_post(self):
+        update_post_url = f'/blog/update_post/{self.post_003.pk}/'
+
+        # 로그아웃 상태
+        response = self.client.get(update_post_url)
+        self.assertNotEqual(response.content, 200)
+
+        # 로그인은 했지만 작성자가 아닌 경우
+        self.assertNotEqual(self.post_003.author, self.user_trump)
+
+        self.client.login(
+            username=self.user_trump.username,
+            password='somepassword',
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 403)
+
+        # 작성자가 접근하는 경우
+        self.client.login(
+            username=self.post_003.author.username,
+            password='somepassword',
+        )
+        response = self.client.get(update_post_url)
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Edit - Blog', soup.title.text)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        response = self.client.post(
+            update_post_url,
+            {
+                'title': '세 번째 포스트를 수정했습니다.',
+                'content': '안녕 세계? 우리는 하나!',
+                'category': self.category_music.pk
+            },
+            follow=True
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
+        self.assertIn('안녕 세계? 우리는 하나!', main_area.text)
+        self.assertIn(self.category_music.name, main_area.text)
 
     def test_create_post(self):
 
@@ -63,7 +107,7 @@ class TestView(TestCase):
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        self.assertEqual('Create Post - Blog', soup.title.text)
+        self.assertEqual('Create - Blog', soup.title.text)
         main_area = soup.find('div', id='main-area')
         self.assertIn('Create New Post', main_area.text)
 
